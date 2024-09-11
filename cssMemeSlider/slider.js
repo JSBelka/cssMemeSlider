0cb5384b2cb68d54c523a433fd3ec7275c613f78
JSBelka <barbi-bar@mail.ru>
2024-09-11 19:14:45 +0300

    feat: add new code (Mon, Sep 11, 2024 07:14:00 PM)

--- cssMemeSlider/slider.js
+++ cssMemeSlider/slider.js
@@ -1,32 +1,16 @@
-let slideIndex = 1;
-let textIndex = 1;
-showSlides(slideIndex, textIndex);
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+const texts = document.querySelectorAll('.text');
+let slideIndex = 0;
 
-function currentSlide(x, y) {
-    showSlides(slideIndex = x, textIndex = y);
-}
-
-function showSlides(x, y) {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("dot");
-    let text = document.getElementsByClassName("text");
-    if (x > slides.length) {slideIndex = 1}
-    if (x < 1) {slideIndex = slides.length}
-
-    if (y > text.length) {textIndex = 1}
-    if (y < 1) {textIndex = text.length}
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < text.length; i++) {
-        text[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    text[textIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        slideIndex = index;
+        slides.forEach(slide => slide.classList.remove('active'));
+        slides[slideIndex].classList.add('active');
+        dots.forEach(dot => dot.classList.remove('active'));
+        dot.classList.add('active');
+        texts.forEach(text => text.classList.remove('active'));
+        texts[slideIndex].classList.add('active');
+    });
+});
